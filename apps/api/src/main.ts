51af7f6b1025dccf640745a2c81374956a74f236
Teenapat Rattanawong <teenapat.rat@gmail.com>
2026-01-14 22:32:47 +0700

    update yml and allow cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://salary-man.teenapat.com'],
     credentials: true,
   });
 
